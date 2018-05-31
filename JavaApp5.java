47d9af024ff000037c2e6051a691ab6c7b984b5f
HAL <halst524@gmail.com>
2018-05-31 11:52:33 +0900

    if文

---
+++ JavaApp5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class JavaApp5 {
+    public static void main(String[] arcs){
+        int i = 1;
+        if(i == 2){
+            System.out.println("プログラミングキャンプ！");
+        } else if(i == 1){
+            System.out.println("1です！");
+        } else {
+            System.out.println("その他です！");
+        }
+    }
+    
+}
