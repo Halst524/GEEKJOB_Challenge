34ddd58c9db423f9a64ac049bfe2ed294bd38e85
HAL <halst524@gmail.com>
2018-05-31 13:24:00 +0900

    switch文

---
+++ JavaApp6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class JavaApp6 {
+    public static void main(String[] arcs){
+        int a = 1;
+        switch(a){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            default:
+                System.out.println("想定外");
+                break;
+        }
+    }
+    
+}
