056a21f9041b019956d3db23d523cba26a08ccf9
HAL <halst524@gmail.com>
2018-05-31 11:33:54 +0900

    文字連結

---
+++ JavaApp4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class JavaApp4 {
+    public static void main(String[] arcs){
+        String f = "groove";
+        String m = "-";
+        String l = "gear";
+        System.out.println(f + m + l);
+        
+    }
+    
+}
